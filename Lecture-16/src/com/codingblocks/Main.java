4b676e8b1969979b7c984ab4f153e010df2cf316
KeenWarrior <anujgargcse@gmail.com>
2018-07-07 12:46:21 +0530

    lec 16

---
+++ Lecture-16/src/com/codingblocks/Main.java
@@ -0,0 +1,15 @@
+package com.codingblocks;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Tree tree = new Tree();
+
+        tree.populate();
+
+        // 5 3 1 0 2 0 11 1 10 0
+
+        tree.display();
+    }
+}
---
+++ Lecture-16/src/com/codingblocks/Tree.java
@@ -0,0 +1,71 @@
+package com.codingblocks;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Tree {
+
+    private Node root;
+
+    public Tree(){
+        // nothing to do here right now
+    }
+
+    public void populate(){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter root node : ");
+        int value = scanner.nextInt();
+
+        root = new Node(value);
+
+        populate(root, scanner);
+
+    }
+
+    public void populate(Node node, Scanner scanner){
+        System.out.println("How many children for " + node.value);
+
+        int count = scanner.nextInt();
+
+        for (int i = 0; i < count; i++) {
+
+            String temp = String.format("Enter %d th child node of %d : ", i, node.value);
+            System.out.println(temp);
+            int value = scanner.nextInt();
+
+            Node child = new Node(value);
+
+            node.children.add(child);
+            child.parent = node;
+
+            populate(child, scanner);
+        }
+
+    }
+
+    public void display(){
+        display(root, "");
+    }
+
+    private void display(Node node, String indent) {
+        System.out.println(indent +  node.value);
+
+        for (int i = 0; i < node.children.size(); i++) {
+            display( node.children.get(i), indent + "\t");
+        }
+    }
+
+
+    private class Node {
+        private int value;
+        private ArrayList<Node> children;
+        private Node parent;
+
+        public Node(int value) {
+            this.value = value;
+            this.children = new ArrayList<>();
+        }
+
+    }
+}
