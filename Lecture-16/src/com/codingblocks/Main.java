983fded5e800b97ea74a4492f45d39cc1fa51fce
KeenWarrior <anujgargcse@gmail.com>
2018-07-10 11:22:25 +0530

    Lect 16 in 17

--- Lecture-16/src/com/codingblocks/Main.java
+++ Lecture-16/src/com/codingblocks/Main.java
@@ -8,10 +8,19 @@ public class Main {
 
         tree.populate();
 
-        // 5 3 1 0 2 0 11 1 10 0
+        // 5 3 1 0 2 0 11 4 10 0 12 0 13 0 14 0
 
-        tree.display();
+//        tree.display();
 
-        System.out.println(tree.count());
+//        System.out.println(tree.count());
+//
+//        System.out.println(tree.height());
+//        System.out.println(tree.depth(tree.getRoot()));
+
+//        tree.printDepth(1);
+
+//        System.out.println(tree.degree(tree.getRoot()));
+
+        tree.preOrder();
     }
 }
--- Lecture-16/src/com/codingblocks/Tree.java
+++ Lecture-16/src/com/codingblocks/Tree.java
@@ -1,7 +1,6 @@
 package com.codingblocks;
 
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
 public class Tree {
 
@@ -56,6 +55,72 @@ public class Tree {
         }
     }
 
+    private void preorder(Node node) {
+        System.out.print(node.value + " ");
+
+        for (int i = 0; i < node.children.size(); i++) {
+            preorder( node.children.get(i));
+        }
+    }
+
+    private void postorder(Node node) {
+
+        for (int i = 0; i < node.children.size(); i++) {
+            postorder( node.children.get(i));
+        }
+
+
+        System.out.print(node.value + " ");
+    }
+
+    public void levelOrder(){
+        Queue<Node> queue = new LinkedList<>();
+
+        queue.add(root);
+
+        while (!queue.isEmpty()){
+            Node node = queue.remove();
+            System.out.println(node.value);
+
+            for (int i = 0; i < node.children.size(); i++) {
+                queue.add(node.children.get(i));
+            }
+        }
+
+    }
+
+    public void preOrder(){
+        Stack<Node> stack = new Stack<>();
+
+        stack.push(root);
+
+        while (!stack.isEmpty()){
+            Node node = stack.pop();
+
+            System.out.println(node.value);
+
+            for (int i = node.children.size() - 1; i >= 0; i--) {
+                stack.add(node.children.get(i));
+            }
+        }
+
+    }
+
+    public void printDepth(int depth){
+        printDepth(root, 0, depth);
+    }
+
+    private void printDepth(Node node, int dep, int target) {
+        if (dep == target) {
+            System.out.println(node.value);
+            return;
+        }
+
+        for (int i = 0; i < node.children.size(); i++) {
+            printDepth( node.children.get(i), dep + 1, target);
+        }
+    }
+
     public int count(){
         return count(root);
     }
@@ -75,8 +140,71 @@ public class Tree {
         return sum;
     }
 
+    public int height(){
+        return depth(root) + 1;
+    }
 
+    public int depth(Node node) {
 
+        if (node.children.size() == 0){
+            return 0;
+        }
+
+        int max = 0;
+
+        for (int i = 0; i < node.children.size(); i++) {
+            int dep = depth( node.children.get(i));
+            if (max < dep){
+                max = dep;
+            }
+        }
+
+        return max + 1;
+    }
+
+    public int degree(Node node) {
+
+        if (node.children.size() == 0){
+            return 0;
+        }
+
+        int max = node.children.size();
+
+        for (int i = 0; i < node.children.size(); i++) {
+            int deg = degree( node.children.get(i));
+            if (max < deg){
+                max = deg;
+            }
+        }
+
+        return max;
+    }
+
+    public int maxSum(Node node) {
+
+        if (node.children.size() == 0){
+            return node.value;
+        }
+
+        int max = node.value;
+
+        for (int i = 0; i < node.children.size(); i++) {
+            max += node.children.get(i).value;
+        }
+
+        for (int i = 0; i < node.children.size(); i++) {
+            int deg = maxSum( node.children.get(i));
+            if (max < deg){
+                max = deg;
+            }
+        }
+
+        return max;
+    }
+
+    public Node getRoot() {
+        return root;
+    }
 
     private class Node {
         private int value;
